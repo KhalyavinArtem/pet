04cda680a37851e0a4298687f382587ba173d36f
akhalyavin <akhalyavin@phoenixit.ru>
2022-06-24 20:54:43 +0300

    1. first commit

---
+++ src/test/java/ru/pet/backend/PetApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.pet.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
